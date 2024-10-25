2f0d4a82209acda27e3986dd04a8ac63ad063d46
Manikanth N <manikanth.nk1@gmail.com>
2024-10-25 10:07:33 +0530

    Modified for MD5 to SHA256 Checksum Athenticcation

--- Tools/scripts/make_abin.sh
+++ Tools/scripts/make_abin.sh
@@ -16,14 +16,17 @@ ABINFILE="$2"
     exit 1
 }
 
-sum=$(md5sum "$BINFILE" | cut -d' ' -f1)
+# sum=$(md5sum "$BINFILE" | cut -d' ' -f1)
+sum=$(sha256sum "$BINFILE" | cut -d' ' -f1)
+
 githash=$(git rev-parse HEAD)
 
-echo "githash $githash md5 $sum"
+# echo "githash $githash md5 $sum"
+echo "githash $githash sha256 $sum"
 
 cat <<EOF > "$ABINFILE"
 git version: $githash
-MD5: $sum
+SHA256: $sum
 --
 EOF
 cat "$BINFILE" >> "$ABINFILE"
